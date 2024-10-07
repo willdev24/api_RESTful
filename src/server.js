bc1c256c1d25b790e1d162ab7629f01ab629796e
willy thiago <128657856+willdev24@users.noreply.github.com>
2024-10-07 19:14:39 -0400

    ok

--- src/server.js
+++ src/server.js
@@ -8,6 +8,13 @@ const app = express()
 app.use(express.json())
 db.conect()
 
+const lista = [
+    "http://127.0.0.1:6000/",
+    "http://127.0.0.1:5511/",
+]
+
+
+
 app.get("/:id?",router)
 app.post("/",router)
 app.put("/:id", router)
