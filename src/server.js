f6e855222c73b5470c76663859b4fe113fff7c8f
willy thiago <128657856+willdev24@users.noreply.github.com>
2024-10-07 19:16:21 -0400

    ok

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const { router } = require("./router/router")
 const db = require("./database/index")
-const { teste } = require("./model/model")
+const cors = require("cors")
 
 const app = express()
+
+//app.use(cors())
 
 app.use(express.json())
 db.conect()
